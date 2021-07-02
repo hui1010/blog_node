eb05f0f8b9d0e3dac92a3dad787e67b67e1b38e5
twr19339 <Huiyi.Skarner@IGT.com>
2021-07-02 17:54:38 +0200

    Lession finished

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const morgan = require('morgan')
 const mongoose = require('mongoose')
 
 const Blog = require('./models/blog')
+const { render } = require('ejs')
 
 // express app
 const app = express() // invode that function to create a instance of an express app, stored in the const app
@@ -22,66 +23,83 @@ app.set('view engine', 'ejs') //by default express and ejs will look for the vie
 
 app.use(morgan('dev'))//other options: 'tiny'
 
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: 'new blog',
-        snippet: 'about my new blog',
-        body: 'more about my new blog'
-    })
-
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/all-blogs', (req, res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('some id')
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
 
 //middleware & static files - images and css files which are public
 app.use(express.static('public'))//pass in a folder name
 
-app.get('/', (req, res) => {//path, function for get(), everything send from the address bar is get method
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Yoshi finds a lot of eggs on his way back home'},
-        {title: 'Mario finds stars', snippet: 'Mario wanted starts and he found quite a lot eventually'},
-        {title: 'Bowser finds mushrooms', snippet: 'Bowser wants to eat mushrooms so he can make mushroom soup'}
-    ]
+//midleware for recieving the form data
+app.use(express.urlencoded({ extended: true })) // extended: true is not necessary, can leave the () empty
 
-    /* EJS */
-    res.render('index', {title: 'Home', blogs}) //first parameter: the page, second parameter: data - an object
+app.get('/', (req, res) => {//path, function for get(), everything send from the address bar is get method
+    res.redirect('/blogs')
 })
+
 app.get('/about', (req, res) => {
     //EJS
     res.render('about', {title: 'About'})
 })
 
+
+// blog routes
+app.get('/blogs', (req, res) => {//path, function for get(), everything send from the address bar is get method
+    Blog.find().sort({createdAt: -1}) // sort from newest to oldest
+        .then((result) => {
+            res.render('index', {title: 'All Blogs', blogs: result})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+
+    /* EJS */
+    res.render('index', {title: 'Home', blogs}) //first parameter: the page, second parameter: data - an object
+})
+
+
 app.get('/blogs/create', (req, res) => {
     res.render('create', {title: 'Create'})
 })
-// app.post()
+app.post('/blogs', (req, res) => {
+    // console.lof(req.body)
+    const blog = new Blog(req.body)
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs')
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.get('/blogs/:id', (req, res) => {//params.samename with what's after :
+    const id = req.params.id
+    Blog.findById(id)
+        .then((result) => {
+            res.render('details', { blog: result, title: 'Blog Details' })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id
+    Blog.findByIdAndDelete(id)
+        .then((result) => {
+            res.json({redirect: '/blogs'})
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+})
 
 // 404 pages
 app.use((req, res) => {
     res.status(404).render('404', {title: '404'})
 })
+
+/**
+ * Route parameters
+ * The variable parts of the route that may change value
+ * eg: localhost:3000/blogs/:id
+ * :id is the parameter, can be accessed by req.params.something 
+ */
