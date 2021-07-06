737944c615fea1835b088c84155c19e0ebf2d4e4
twr19339 <Huiyi.Skarner@IGT.com>
2021-07-06 13:58:53 +0200

    Added controllers and blogs folder

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,59 +1,14 @@
 const express = require('express')
-const Blog = require('../models/blog')
+
+const blogController = require('../controllers/blogController')
 
 // create a new express router
 const router = express.Router()
 
-
-router.get('/', (req, res) => {//path, function for get(), everything send from the address bar is get method
-    Blog.find().sort({createdAt: -1}) // sort from newest to oldest
-        .then((result) => {
-            res.render('index', {title: 'All Blogs', blogs: result})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-
-    /* EJS */
-    res.render('index', {title: 'Home', blogs}) //first parameter: the page, second parameter: data - an object
-})
-
-
-router.get('/create', (req, res) => {
-    res.render('create', {title: 'Create'})
-})
-router.post('/blogs', (req, res) => {
-    // console.lof(req.body)
-    const blog = new Blog(req.body)
-    blog.save()
-        .then((result) => {
-            res.redirect('/blogs')
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-router.get('/:id', (req, res) => {//params.samename with what's after :
-    const id = req.params.id
-    Blog.findById(id)
-        .then((result) => {
-            res.render('details', { blog: result, title: 'Blog Details' })
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-router.delete('/:id', (req, res) => {
-    const id = req.params.id
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({redirect: '/blogs'})
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
+router.get('/', blogController.blog_index)
+router.post('/blogs', blogController.blog_create_post)
+router.get('/create', blogController.blog_create_get)
+router.get('/:id', blogController.blog_details)
+router.delete('/:id', blogController.blog_delete)
 
 module.exports = router
